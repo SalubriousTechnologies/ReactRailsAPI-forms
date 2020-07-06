d216d7cec8ff372388c3c94df2df8b511329a922
STan Dvpmnt <standvpmnt@gmail.com>
2020-07-06 12:03:37 +0530

    Added ability to direct upload files from the react front-end, leveraging the direct upload functionality provided by @rails/activestorage

--- src/form_page/doctor_form.component.jsx
+++ src/form_page/doctor_form.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState, createRef } from "react";
+import { DirectUpload } from "@rails/activestorage";
 
 const DoctorForm = ({ redirect }) => {
   const [doctor, setDoctor] = useState({
@@ -41,9 +42,35 @@ const DoctorForm = ({ redirect }) => {
   };
 
   const fileInput = createRef();
+  const handleFile = event => {
+    for (let i = 0; i < fileInput.current.files.length; i++) {
+      uploadFile(fileInput.current.files[i], i);
+    }
+  };
+
+  const uploadFile = (file, index) => {
+    const URL =
+      "http://form-rails-api.herokuapp.com/rails/active_storage/direct_uploads";
+    const upload = new DirectUpload(file, URL);
+
+    upload.create((error, blob) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const hiddenField = document.createElement("input");
+        hiddenField.setAttribute("type", "hidden");
+        hiddenField.setAttribute("value", blob.signed_id);
+        hiddenField.id = `document_${index}`;
+        document.querySelector("form").appendChild(hiddenField);
+      }
+    });
+  };
   const addDocumentsToParams = formData => {
     for (let i = 0; i < fileInput.current.files.length; i++) {
-      formData.append("doctor[documents][]", fileInput.current.files[i]);
+      formData.append(
+        "doctor[documents][]",
+        document.getElementById(`document_${i}`).value
+      );
     }
   };
 
@@ -100,7 +127,12 @@ const DoctorForm = ({ redirect }) => {
       />
       <br />
       <label>Images & Documents</label>
-      <input id="file-uploads" type="file" multiple={true} ref={fileInput} />
+      <input
+        type="file"
+        multiple={true}
+        ref={fileInput}
+        onChange={handleFile}
+      />
       <br />
       <input type="submit" value="Submit" />
     </form>
